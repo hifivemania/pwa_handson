4ca069a70da7a427ff81668c552188f9832b7064
Atsushi <atsushi@moongift.jp>
2018-11-18 11:56:42 +0900

    base javascript file updated.

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,6 +1,15 @@
 var TODO = 'TODO';
 var DOMAIN = 'https://well-silk.glitch.me';
 
+// オフライン時に追加/削除するTodo入れておくキュー
+var queues = {add: [], delete: []};
+for (var name of ['add', 'delete']) {
+  var value = localStorage.getItem(name + 'Queue');
+  if (value) {
+    queues[name] = JSON.parse(value);
+  }
+}
+
 $(function() {
   if (TODO === 'TODO') {
     alert('変数TODOを変更してください');
@@ -26,57 +35,115 @@ $(function() {
       .then(function(result) {
         // 結果を変数に入れておく
         me.__todos = result;
-        // 表示処理
-        me.view.append('#list', 'listTemplate', {
-          todos: me.__todos
-        });
+        // 表示を更新
+        me.updateView();
+        
+        // キューを処理
+        if (navigator.onLine) {
+          me.executeQueue(queues);
+        }
       })
     },
-    
     // Todoの登録処理
     'button click': function(context, $el) {
       context.event.preventDefault();
       var me = this;
       // 登録するTodo
       var todo = this.$find('input#inputTodo').val();
-      // サーバに登録
-      $.ajax({
-        url: DOMAIN + '/todos/' + TODO,
-        type: 'POST',
-        data: {
-          todo: todo
-        }
-      })
-      .then(function(result) {
-        // サーバから取得したTodoを追加
-        me.__todos.push(result.todo);
-        // 表示を更新
-        me.view.update('#list', 'listTemplate', {
-          todos: me.__todos
+      this.addTodo(todo)
+        .then(function(result) {
+          // サーバから取得したTodoを追加
+          me.__todos.push(result.todo);
+          // 表示を更新
+          me.updateView();
+          // 入力欄を消す
+          me.$find('input#inputTodo').val('');
         });
-        // 入力欄を消す
-        me.$find('input#inputTodo').val('');
-      })
     },
     // Todoを削除する処理
     '.delete click': function(context, $el) {
       var todo = $el.data('todo');
       var me = this;
-      // サーバに送信
-      $.ajax({
-        url: DOMAIN + '/todos/' + TODO + '/' + todo,
-        type: 'DELETE'
-      })
-      .then(function(result) {
-        // 変数からタスクを消す
-        me.__todos = me.__todos.filter(function(t) {
-          return todo != t;
+      this.deleteTodo(todo)
+        .then(function(result) {
+          // 変数からタスクを消す
+          me.__todos = me.__todos.filter(function(t) {
+            return todo != t;
+          });
+          // 表示を更新
+          me.updateView();
+        })
+    },
+    // Todo追加、削除の実行
+    executeTask: function(action, todo) {
+      switch (action) {
+      case 'add':
+        this.addTodo(todo);
+        break;
+      case 'delete':
+        this.deleteTodo(todo);
+        break;
+      }
+    },
+    // キューを処理する
+    executeQueue: function(queues) {
+      var me = this;
+      for (var action of ['add', 'delete']) {
+        for (var todo of queues[action]) {
+          this.executeTask(action, todo);
+        }
+        queues[action] = [];
+        localStorage.setItem(action + 'Queue', []);
+      }
+    },
+    // 表示を更新する処理
+    updateView: function() {
+      // 表示を更新
+      this.view.update('#list', 'listTemplate', {
+        todos: this.__todos
+      });
+      // Service Workerのキャッシュを更新します（第5章）
+    },
+    // Todoを追加する処理
+    addTodo: function(todo) {
+      return new Promise(function(res, rej) {
+        // オフライン時の処理（第6章で追加）
+        
+        // オンライン時の処理
+        // サーバに登録
+        $.ajax({
+          url: '/todos/' + TODO,
+          type: 'POST',
+          data: {
+            todo: todo
+          }
+        })
+        .then(function(result) {
+          res(result)
         });
-        // 表示を更新
-        me.view.update('#list', 'listTemplate', {
-          todos: me.__todos
-        });
       })
+    },
+    // Todoを削除する処理
+    deleteTodo: function(todo) {
+      return new Promise(function(res, rej) {
+        // オフライン時
+        if (!navigator.onLine) {
+          queues.delete.push(todo);
+          localStorage.setItem('deleteQueue', JSON.stringify(queues.delete));
+          return res({todo});
+        }
+        // オンライン時はサーバに送信
+        $.ajax({
+          url: '/todos/' + TODO + '/' + todo,
+          type: 'DELETE'
+        })
+        .then(function(result) {
+          return res(todo);
+        })
+      })
+    },
+    // オンライン復帰時の処理（第6章用）
+    '{window} online': function(context) {
     }
   };
   h5.core.controller('.container', todoController);
